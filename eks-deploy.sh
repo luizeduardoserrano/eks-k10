2d6a9e5f0a14c33004ac692ab074906ea07ce54d
Luiz E Serrano <leduardoserrano@gmail.com>
2023-08-26 09:34:08 -0300

    Elminado comentário

--- eks-deploy.sh
+++ eks-deploy.sh
@@ -29,6 +29,5 @@ endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
 echo "-------Total time to build an EKS cluster is $(($duration / 60)) minutes $(($duration % 60)) seconds."
 echo "" | awk '{print $1}'
-echo "-------Created by Yongkang"
-echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "-------Created"
 echo "" | awk '{print $1}'
